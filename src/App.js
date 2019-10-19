43097429dc00c00e9282ac2230cfa5bc6feb5f8b
Preetham Sridhar <preetham.prem2325@gmail.com>
2019-10-19 18:47:45 +0530

    Flexible lists, rendering changes accordingly

--- src/App.js
+++ src/App.js
@@ -23,14 +23,25 @@ class App extends Component {
 		})
 	}
 
-	nameChangedHandler = (event) => {
-		this.setState({
-			persons: [
-				{ id:'sdadfga', name: "Max", age: 10 },
-				{ id:'ksdfafg', name: event.target.value, age: 27 },
-				{ id:'tuiario', name: 'Stephanie', age: 30 }
-			]
+	nameChangedHandler = (event, id) => {
+		const personIndex = this.state.persons.findIndex( p => {
+			return p.id === id;
 		})
+		console.log(personIndex)
+
+		const person = {
+			...this.state.persons[personIndex]
+		};
+
+		person.name = event.target.value;
+
+		const persons = [...this.state.persons]
+		persons[personIndex] = person;
+
+		this.setState({
+			persons: persons
+		});
+
 	}
 
 	togglePersonsHandler = () => {
@@ -50,15 +61,15 @@ class App extends Component {
 		if (this.state.showPersons) {
 			persons = (
 				<div>
-					{this.state.persons.map( (person, index) => {
+					{ this.state.persons.map((person, index) => {
 						return (
 							<Person 
-								click={this.deletePersonHandler.bind(this, index)}
-								name={person.name}
-								age={person.age}
-								key={person.id}
-								changed={this.nameChangedHandler}
-								/>
+								click = {this.deletePersonHandler.bind(this, index)}
+								name = { person.name }
+								age = { person.age }
+								key = { person.id }
+								changed = { (event) => this.nameChangedHandler(event, person.id) }
+							/>
 						);
 					})}
 				</div> 
